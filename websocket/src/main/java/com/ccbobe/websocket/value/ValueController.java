8c3a7d12fd113540b348f676b4ca3f74f6be9494
蒙旺旺 <1132717195@qq.com>
2019-12-26 16:18:53 +0800

    删除无用引用代码

--- websocket/src/main/java/com/ccbobe/websocket/value/ValueController.java
+++ websocket/src/main/java/com/ccbobe/websocket/value/ValueController.java
@@ -3,7 +3,6 @@ package com.ccbobe.websocket.value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
