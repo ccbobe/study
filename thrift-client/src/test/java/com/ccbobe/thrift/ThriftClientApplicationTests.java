4e2920a9750cfcb77fcb9d8eac6edefb622181cf
蒙旺旺 <1132717195@qq.com>
2020-04-02 09:10:50 +0800

    '新增测试用例'

--- thrift-client/src/test/java/com/ccbobe/thrift/ThriftClientApplicationTests.java
+++ thrift-client/src/test/java/com/ccbobe/thrift/ThriftClientApplicationTests.java
@@ -8,6 +8,21 @@ class ThriftClientApplicationTests {
 
     @Test
     void contextLoads() {
+        Integer integer = testFinally();
+        System.out.println(integer);
+    }
+
+
+    public Integer testFinally(){
+        try {
+            Integer count = 0;
+            count = 100/1000;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new NumberFormatException();
+        } finally {
+            return -2;
+        }
     }
 
 }
